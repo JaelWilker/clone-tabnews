5383e017d980201c04440aee965540d21dd3c591
Jael Wilker <119270064+JaelWilker@users.noreply.github.com>
2025-04-24 20:01:26 +0000

    add database.js and .env to the project

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,12 @@
-function status(request, response) {
-  response
-    .status(200)
-    .json({
-      chave:
-        "Quem é minha Harpista mais linda desse mundo em em, quem é minha malevolazinhameudeus",
-    });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 AS result;");
+  console.log(result.rows);
+  response.status(200).json({
+    chave:
+      "Quem é minha Harpista mais linda desse mundo em em, quem é minha malevolazinhameudeus",
+  });
 }
 
 export default status;
